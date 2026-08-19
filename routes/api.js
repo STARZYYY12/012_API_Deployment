cdaae9750b8e32f6a43c3d4e9216f6449795a204
STARZYYY12 <rizkyfaiz324@gmail.com>
2026-08-19 15:26:52 +0700

    fix: correct casing in userController import path in api.js

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const komikController = require("../controller/komikController");
-const userController = require("../controller/userController");
+const userController = require("../controller/UserController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/register", userController.register);
