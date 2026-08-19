8af3482d916af67857234d419514d150a2a8df96
STARZYYY12 <rizkyfaiz324@gmail.com>
2026-08-19 15:13:04 +0700

    fix: add missing semicolon and improve formatting in config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
-require("dotenv").config();
 
+Config.JS
+require("dotenv").config();
+ 
 const development = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
@@ -7,9 +9,26 @@ const development = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: process.env.DB_DIALECT,
-}
-
+};
+ 
+const production = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+  // Supabase mewajibkan koneksi SSL dari luar
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+};
+ 
 module.exports = {
   development,
-  production
+  production,
 };
+ 
