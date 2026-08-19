f76ec179981a0ba81e0d481444eb89a27e52bbd8
STARZYYY12 <rizkyfaiz324@gmail.com>
2026-08-19 16:10:51 +0700

    feat: add genre routes for CRUD operations in api.js

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const komikController = require("../controller/komikController");
 const userController = require("../controller/UserController");
+const genreController = require("../controller/genreController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/register", userController.register);
@@ -11,10 +12,16 @@ router.post("/login", userController.login);
 // Public
 router.get("/komik", komikController.getAllKomik);
 router.get("/komik/:id", komikController.getKomikById);
+router.get("/genre", genreController.getAllGenre);
+router.get("/genre/:id", genreController.getGenreById);
 
 // Protected
 router.post("/komik", authMiddleware, komikController.createKomik);
 router.put("/komik/:id", authMiddleware, komikController.updateKomik);
 router.delete("/komik/:id", authMiddleware, komikController.deleteKomik);
+
+router.post("/genre", authMiddleware, genreController.createGenre);
+router.put("/genre/:id", authMiddleware, genreController.updateGenre);
+router.delete("/genre/:id", authMiddleware, genreController.deleteGenre);
 
 module.exports = router;
