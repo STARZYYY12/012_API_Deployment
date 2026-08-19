9938c02ef94d14602b11254d0955ed8fb55193e6
STARZYYY12 <rizkyfaiz324@gmail.com>
2026-08-19 15:22:43 +0700

    refactor: clean up formatting and remove unnecessary comments in config.js

--- config/config.js
+++ config/config.js
@@ -1,34 +1,25 @@
-
-Config.JS
 require("dotenv").config();
- 
+
 const development = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT,
+username: process.env.DB_USER,
+password: process.env.DB_PASS,
+database: process.env.DB_DATABASE,
+host: process.env.DB_HOST,
+port: process.env.DB_PORT,
+dialect: process.env.DB_DIALECT
 };
- 
+
 const production = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT,
-  // Supabase mewajibkan koneksi SSL dari luar
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
+    use_env_variable: "POSTGRES_URL",
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
 };
- 
+
 module.exports = {
-  development,
-  production,
+    development,
+    production
 };
- 
